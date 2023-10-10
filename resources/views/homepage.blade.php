2e678d8beb7d2d9d04a1f47bb50d72fbf755f7a2
Matt DeClaire <matt@declaire.com>
2023-10-09 21:26:18 -0700

    more homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,12 +5,23 @@
         <h1>Experience professional design every day in your own home.</h1>
     </x-section>
 
-    <x-section>
+    <x-section id="about">
+        <h2>I'm Katie DeClaire.</h2>
+        <p>I'm an interior designer in the San Francisco Bay Area, specializing in home furnishings and decor. With my background in merchandising, I can help you transform your home into a space you love to come home to every day.</p>
+    </x-section>
+
+    <x-section id="testimonials">
         @foreach($testimonials as $testimonial)
-            <blockquote class="testimonial">
+            <blockquote>
                 <p>{{$testimonial['content']}}</p>
                 <cite>{{$testimonial['name']}}</cite>
             </blockquote>
         @endforeach
     </x-section>
+
+    <x-section id="home-contact">
+        <p>Reach out today, and let's get started.</p>
+        <a href="/contact" class="btn">Contact Me</a>
+    </x-section>
+
 @endsection
