3d65e5d76aa63a427c4da97e92471777b926e68f
Matt DeClaire <matt@declaire.com>
2023-10-09 20:35:58 -0700

    mobile nav

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -13,7 +13,14 @@
                 <a href="/" id="logo">DeClaire Design</a>
 
                 <nav id="site-nav">
+                    <!-- mobile toggle and hamburger -->
+                    <input type="checkbox" id="nav-toggle" />
+                    <span class="mobile-hamburger"></span>
+                    <span class="mobile-hamburger"></span>
+                    <span class="mobile-hamburger"></span>
+
                     <ol>
+                        <li><a href="/">Home</a></li>
                         <li><a href="/about">About</a></li>
                         <li><a href="/portfolio">Portfolio</a></li>
                         <li><a href="/process">Process</a></li>
