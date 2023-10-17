cda96d4deab71bfafd684f83a2d5e801cdb85e05
Matt DeClaire <matt@declaire.com>
2023-10-16 21:55:33 -0700

    comment out placeholder testimonials

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -10,9 +10,9 @@ class PublicController extends Controller
     {
         return view('homepage', [
             'testimonials' => [
-                ['name' => "The Murrays", 'content' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor corporis possimus totam eligendi officia laboriosam sed ut ex esse, numquam soluta pariatur repudiandae dolorem a illo, officiis sint assumenda repellat."],
-                ['name' => "The Murrays", 'content' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor corporis possimus totam eligendi officia laboriosam sed ut ex esse, numquam soluta pariatur repudiandae dolorem a illo, officiis sint assumenda repellat."],
-                ['name' => "The Murrays", 'content' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor corporis possimus totam eligendi officia laboriosam sed ut ex esse, numquam soluta pariatur repudiandae dolorem a illo, officiis sint assumenda repellat."],    
+                // ['name' => "The Murrays", 'content' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor corporis possimus totam eligendi officia laboriosam sed ut ex esse, numquam soluta pariatur repudiandae dolorem a illo, officiis sint assumenda repellat."],
+                // ['name' => "The Murrays", 'content' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor corporis possimus totam eligendi officia laboriosam sed ut ex esse, numquam soluta pariatur repudiandae dolorem a illo, officiis sint assumenda repellat."],
+                // ['name' => "The Murrays", 'content' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor corporis possimus totam eligendi officia laboriosam sed ut ex esse, numquam soluta pariatur repudiandae dolorem a illo, officiis sint assumenda repellat."],    
             ],
         ]);
     }
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -10,14 +10,16 @@
         <p>I'm an interior designer in the San Francisco Bay Area, specializing in home furnishings and decor. With my background in merchandising, I can help you transform your home into a space you love to come home to every day.</p>
     </x-section>
 
-    <x-section id="testimonials">
-        @foreach($testimonials as $testimonial)
-            <blockquote>
-                <p>{{$testimonial['content']}}</p>
-                <cite>{{$testimonial['name']}}</cite>
-            </blockquote>
-        @endforeach
-    </x-section>
+    @if (count($testimonials))
+        <x-section id="testimonials">
+            @foreach($testimonials as $testimonial)
+                <blockquote>
+                    <p>{{$testimonial['content']}}</p>
+                    <cite>{{$testimonial['name']}}</cite>
+                </blockquote>
+            @endforeach
+        </x-section>
+    @endif
 
     <x-section id="home-contact">
         <p>Reach out today, and let's get started.</p>
